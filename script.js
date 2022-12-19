8b262b29749ddc73e27dbbd6a1e60413415c82be
J <dpsdamagepersecond@outlook.com>
2022-12-19 15:55:46 -0800

    FINISHED, GOOD

--- script.js
+++ script.js
@@ -2,10 +2,23 @@ const number = document.querySelectorAll('.number');
 const operator = document.querySelectorAll('.operator');
 const head = document.querySelector('.head');
 const equal = document.querySelector('.equal');
+const ce = document.querySelector('.clear');
+const source = document.querySelector('.source')
 
 let tempOne;
 let tempTwo;
 let tempOp;
+
+const operators = {
+    '+': function(a, b) { return +a + +b },
+    '-': function(a, b) { return a - b },
+    '*': function(a, b) { return a * b },
+    '/': function(a, b) { return a / b },
+}
+
+document.addEventListener('click', () => {
+    source.innerHTML = `${tempOne}, ${tempOp}, ${tempTwo}`;
+});
 
 number.forEach((number) => {
     number.addEventListener('click', () => {
@@ -15,13 +28,27 @@ number.forEach((number) => {
 
 operator.forEach((operator) => {
     operator.addEventListener('click', () => {
-        tempOp = operator.innerHTML;
         if(tempOne === undefined) {
             tempOne = head.innerHTML;
-        } else {
-            tempTwo = head.innerHTML;
         };
+
+        tempOp = operator.innerHTML;
         head.innerHTML = '';
-    })
-})
+    });
+});
+
+equal.addEventListener('click', () => {
+    tempTwo = head.innerHTML;
+    head.innerHTML = operators[tempOp](tempOne, tempTwo);
+    tempOne = head.innerHTML;
+    tempTwo = 0;
+    tempOp = undefined;
+});
+
+ce.addEventListener('click', () => {
+    tempOne = undefined;
+    tempTwo = undefined;
+    tempOp = undefined;
+    head.innerHTML = '';
+});
 
