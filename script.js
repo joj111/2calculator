538302fd764e70c30ae017e349d13c02f7b1b555
J <dpsdamagepersecond@outlook.com>
2022-12-21 12:25:11 -0800

    added comments

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let tempOne;
 let tempTwo;
 let tempOp;
 
+//magic
 const operators = {
     '+': function(a, b) { return +a + +b },
     '-': function(a, b) { return a - b },
@@ -16,27 +17,38 @@ const operators = {
     '/': function(a, b) { return a / b },
 }
 
-document.addEventListener('click', () => {
+//showing the variables each time one is changed
+document.addEventListener('click', () => { 
     source.innerHTML = `${tempOne}, ${tempOp}, ${tempTwo}`;
 });
 
+//when number key pressed, put in head
 number.forEach((number) => {
     number.addEventListener('click', () => {
         head.innerHTML += number.innerHTML;
     });
 });
 
+//checking if has pre-existing variable one and also confirms operator
 operator.forEach((operator) => {
     operator.addEventListener('click', () => {
         if(tempOne === undefined) {
             tempOne = head.innerHTML;
         };
+        //scrapped doesn't work, if variable all defined, does equal then resets
+        // if(tempTwo != undefined) { 
+        //     head.innerHTML = operators[tempOp](tempOne, tempTwo);
+        //     tempOne = head.innerHTML;
+        //     tempTwo = 0;
+        //     tempOp = undefined;
+        // };
 
         tempOp = operator.innerHTML;
         head.innerHTML = '';
     });
 });
 
+//evaluates variables
 equal.addEventListener('click', () => {
     tempTwo = head.innerHTML;
     head.innerHTML = operators[tempOp](tempOne, tempTwo);
@@ -45,6 +57,7 @@ equal.addEventListener('click', () => {
     tempOp = undefined;
 });
 
+//clear variables
 ce.addEventListener('click', () => {
     tempOne = undefined;
     tempTwo = undefined;
